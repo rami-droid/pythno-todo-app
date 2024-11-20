25452aaa93eb98c7f5467335e594354ee71fdfcf
rami-droid <e-ramabb@iskule.no>
2024-11-20 13:35:07 +0100

    add button

--- todo.py
+++ todo.py
@@ -1,14 +1,21 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton
+from PyQt5.QtCore import pyqtSlot
 
 app = QApplication([])
+
+@pyqtSlot()
+def on_click():
+    print("Hellor World")
 
 window = QWidget()
 window.setWindowTitle("PyQt App")
 window.setGeometry(750, 300, 500, 500)
-helloMsg = QLabel("Todo App", parent=window)
+helloMsg = QLabel("<h1>Todo App</h1>", parent=window)
 helloMsg.move(60, 15)
+button = QPushButton(text="button", parent=window)
+
 
 window.show()
 
