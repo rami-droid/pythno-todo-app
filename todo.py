653de590d858b420e949826771eae5ba840d5002
rami-droid <e-ramabb@iskule.no>
2024-11-21 10:52:38 +0100

    add input field and array

--- todo.py
+++ todo.py
@@ -1,13 +1,21 @@
 from tkinter import *
 
 def on_button_click():
-    print("button clicked!")
+    todos.append(todoEntry.get())
+    todoEntry.select_clear()
+    print(todos)
 
+todos = []
 
 root = Tk()
+root.title = "todo app"
+root.minsize(500, 500)
 
 button = Button(root, text="click me", command=on_button_click)
 
+todoEntry = Entry(root)
+
 button.pack(padx=20, pady=20)
+todoEntry.pack()
 
 root.mainloop()
