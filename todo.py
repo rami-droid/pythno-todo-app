6bb8498475b30bcb14bfdcd816cb4c82bef3a241
rami-droid <e-ramabb@iskule.no>
2024-11-21 13:05:39 +0100

    work on delete from list feat

--- todo.py
+++ todo.py
@@ -1,11 +1,18 @@
 from tkinter import *
 
+def KBDaddToList(e):
+    on_button_click()
+
 #adds input to list and sends to display
-def on_button_click(self):
+def on_button_click():
     if todoEntry.get() != "":
         todos.append(todoEntry.get())
         todoText.insert(END, todoEntry.get() + "\n")
         todoEntry.delete(0, len(todoEntry.get()))
+
+#deletes top todo from list
+def on_delete_click():
+    return
 
 todos = []
 
@@ -16,7 +23,9 @@ root.minsize(500, 500)
 
 #add to list button
 button = Button(root, text="add to list", command=on_button_click)
-root.bind('<Return>', on_button_click)
+root.bind('<Return>', KBDaddToList)
+
+deleteBtn = Button(root, text="delete from list", command=on_delete_click)
 
 
 exitBtn = Button(root, text="exit", command=root.destroy)
@@ -30,9 +39,11 @@ todoText = Text(displayFrame)
 
 #pack everything in windows
 button.pack()
+deleteBtn.pack()
 exitBtn.place(x=600, y=1)
 todoEntry.pack()
 displayFrame.pack()
 todoText.pack()
+
 
 root.mainloop()
