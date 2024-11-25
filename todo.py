4f573b9579d1c2f93502feceaa7719d8fac27214
rami-droid <e-ramabb@iskule.no>
2024-11-25 11:35:18 +0100

    reworking classes

--- todo.py
+++ todo.py
@@ -4,15 +4,28 @@ def KBDaddToList(e):
     on_button_click()
 
 class todo:
-    label = Label
-    text = Text
-    deleteBtn = Button(text="delete")
+    def __init__(self, frame, parent, todoText) -> None:
+        self.frame = Frame(parent)
+        self.label = Label(self.frame, text=todoText)
+        self.text = todoText
+        self.deleteBtn = Button(text="delete")
+
+        self.label.pack(side=LEFT)
+        self.deleteBtn.pack(side=RIGHT)
+ 
 #adds input to list and sends to display
 def on_button_click():
     if todoEntry.get() != "":
-        todos.append(todoEntry.get())
-        todoText.insert(END, todoEntry.get() + "\n")
-        todoEntry.delete(0, len(todoEntry.get()))
+        todos.append(todo(todoEntry.get()))
+        print(todos[len(todos) - 1].text)
+
+        todos[len(todos) - 1].pack()
+        
+
+        # todos.append(todoEntry.get())
+        # todoText.insert(END, todoEntry.get() + "\n")
+        # todoEntry.delete(0, len(todoEntry.get()))
+
 
 #deletes top todo from list
 def on_delete_click():
@@ -38,7 +51,7 @@ todoEntry = Entry(root)
 
 #output for todo list
 displayFrame = Frame()
-todoText = Text(displayFrame)
+# todoText = Text(displayFrame)
 
 
 #pack everything in windows
@@ -47,7 +60,9 @@ deleteBtn.pack()
 exitBtn.place(x=600, y=1)
 todoEntry.pack()
 displayFrame.pack()
-todoText.pack()
+
+for todo in todos:
+    todo.pack(displayFrame)
 
 
 root.mainloop()
